4768d652ba7e02063bf8453d7494e0f6f9828608
Retr0-46 <88389726+Retr0-46@users.noreply.github.com>
2022-04-09 10:23:07 +0300

    больше улучшений!

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import keyboard
 import pyperclip
 import os
-#import win10toast
+import win10toast
 
-#tost = win10toast.ToastNotifler()
+tost = win10toast.ToastNotifier()
 all_data = []
+
 
 def f():
     data = pyperclip.paste()
     all_data.append(data)
-    #tost.show_toast(f'F{len(all_data)}',f'добавлено: {data}', duration=4)
-    print(f'F{len(all_data)}' + f' добавлено: {data}')
-    keyboard.add_abbreviation(f'F{len(all_data)}', f'{all_data[len(all_data) -1]}')
-    
-keyboard.add_hotkey("shift+space", f)
+    tost.show_toast(f'F{len(all_data)}', f'добавлено: {data}', duration=4)
+    keyboard.add_abbreviation(f'F{len(all_data)}', f'{all_data[len(all_data) - 1]}')
+    return 0
+
+
+while True:
+    if keyboard.is_pressed('shift+space'):
+        f()
     
