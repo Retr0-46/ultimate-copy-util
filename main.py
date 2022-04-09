69c05eea4a9dd9f88f5088b83226d2ea5c8bf83e
Retr0-46 <88389726+Retr0-46@users.noreply.github.com>
2022-04-09 09:43:29 +0300

    Update main.py
    при копировании текста и нажатии 1+esc, данные вводятся в расширенный буфер обмена. их можно достать от туда введя F+порядковый номер данных+пробел

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import keyboard
 import pyperclip
 
+all_data = []
+
 def f():
     data = pyperclip.paste()
-    print(data)
-
-
+    all_data.append(data)
+    print(f'добавлено: {data}')
+    keyboard.add_abbreviation(f'F{len(all_data)}', f'{all_data[len(all_data) -1]}')
+    
 keyboard.add_hotkey("1+esc", f)
+    
